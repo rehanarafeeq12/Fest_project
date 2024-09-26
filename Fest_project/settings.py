0f4f7ae468474f87d8c90a0516cba056cfc9d0ca
rehanarafeeq12 <frehanaky@gmail.com>
2024-09-26 12:12:47 +0530

    'msg'

--- Fest_project/settings.py
+++ Fest_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f0c0&kt&ion1b4$2*x9qq-q8leojtgrr015t5_m=$-j-6f_^ny'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
